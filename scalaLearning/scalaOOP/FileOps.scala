7b0ad686ee8a2792c463c9c914b16ce2a8f82de6
sparkLiwei <sevenyearonelife@foxmail.com>
2016-03-17 21:26:51 +0800

    update some files

---
+++ scalaLearning/scalaOOP/FileOps.scala
@@ -0,0 +1,26 @@
+import java.io.{File => JFile}
+
+/**
+ * Created by Variant on 16/3/17.
+ */
+object FileOps {
+  def main(args: Array[String]) {
+    //scala里的io操作很多是采用java的io操作
+//    val file = Source.fromFile("E:\\sparkKiwe.txt")
+//    for (line <- file.getLines()){println(line)}
+//    file.close
+//
+//    val webFile = Source.fromURL("http://spark.apache.org/")
+//    webFile.foreach(print(_))
+//    webFile.close
+//
+//    val writer = new PrintWriter(new JFile("scalaFile.txt"))
+//      for (i <- 1 to 100) writer.println(i)
+//      writer.close
+
+    print("please enter your input :")
+    val line = Console.readLine()
+    println("Thanks,you just typed:" + line)
+
+  }
+}
---
+++ scalaLearning/scalaOOP/RegExpressOps.scala
@@ -0,0 +1,32 @@
+
+
+/**
+ * Created by Variant on 16/3/17.
+ */
+object RegExpressOps {
+  def main(args: Array[String]) {
+
+    val regex = """([0-9]+) ([a-z]+)""".r
+    val numPattern = "[0-9]+".r
+    val numberPattern = """\s+[0-9]+\s""".r
+
+    //findAllIn 方法返回遍历所有匹配项的迭代器
+    for (matchingString <- numPattern.findAllIn("99345 Scala, 22298 Spark")) println(matchingString)
+
+    //找到首个匹配项
+    println(numberPattern.findFirstIn("99ss java, 222 hadoop"))
+
+    val numitemPattern ="""([0-9]+) ([a-z]+)""".r
+
+    //不是很明白,这东西干嘛的
+    //val numitemPattern(num,item) = "99 hadoop"
+
+    //生产环境下用,模式匹配与正则表达式
+    val line = "98989 spark"
+    line match {
+      case numitemPattern(num,item) => println(num + "\t" + item)
+      case _ => println("ohh...")
+    }
+
+  }
+}
