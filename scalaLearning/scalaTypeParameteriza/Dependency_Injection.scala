019788bd341a0c18fa556a017b0b80789bcc67f4
sparkLiwei <sevenyearonelife@foxmail.com>
2016-04-02 23:26:58 +0800

    add something

--- scalaLearning/scalaTypeParameteriza/Dependency_Injection.scala
+++ scalaLearning/scalaTypeParameteriza/Dependency_Injection.scala
@@ -2,18 +2,18 @@
  * Created by Variant on 16/3/27.
  */
 
-trait Logger{
+trait Logger1{
   def log(msg :String)
 }
 trait Auth{
   //此处还可以添加方法要求,鸭子类型
-  auth : Logger =>
+  auth : Logger1 =>
   def act(msg : String){
     log(msg)
   }
 }
 
-object DI extends Auth with Logger{
+object DI extends Auth with Logger1{
   //这样通过类型的限定和方法传入的要求,比java来写依赖注入要方便许多
   //也是self_type应用的典范
   override def log(msg :String){
